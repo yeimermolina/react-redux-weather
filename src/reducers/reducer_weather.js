7e6490cdfc57d61f73c200616a866a88dc43a7f8
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-11-09 19:12:22 -0400

    Add reducers

---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,13 @@
+import { FETCH_WEATHER } from '../actions/index';
+
+
+export default function(state = [], action){
+  console.log(action.type)
+  console.log(FETCH_WEATHER)
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // return state.concat([action.payload.data] );
+      return [action.payload.data, ...state];
+  }
+  return state
+}
