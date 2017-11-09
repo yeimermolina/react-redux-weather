cf04d0e33431384b911e96bc7511dd6648d62348
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-11-09 19:12:55 -0400

    Add actions

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// const createStoreWithMiddleware = applyMiddleware(ReduxPromise, composeWithDevTools)(createStore);
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(...reducers, ReduxPromise),
+));
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
   , document.querySelector('.container'));
