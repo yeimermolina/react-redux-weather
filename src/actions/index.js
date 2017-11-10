4c96e41af95dd116cc40c91ed30be42807ce5dcc
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-11-10 16:03:37 -0400

    Minor update on actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,17 @@
-import axios from 'axios';
-const API_KEY = '9087c48d533df81fa461f7a071516e3f';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+import axios from "axios";
 
-export const FETCH_WEATHER = 'FETCH_WEATHER';
+const API_KEY = "9087c48d533df81fa461f7a071516e3f";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
-export function fetchWeather(city="NEW YORK"){
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city="new york") {
   const url = `${ROOT_URL}&q=${city},us`;
-  
   const request = axios.get(url);
-  
+
   return {
     type: FETCH_WEATHER,
     payload: request
   };
 }
+
