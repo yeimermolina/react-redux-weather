505f6e1465ad70023a994e0d87562aa5aac247ed
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-11-10 16:04:11 -0400

    Prevents add null data to state

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -7,7 +7,10 @@ export default function(state = [], action){
   switch (action.type) {
     case FETCH_WEATHER:
       // return state.concat([action.payload.data] );
-      return [action.payload.data, ...state];
+      if (action.payload.data){
+        return [action.payload.data, ...state];
+      }
+      return state
   }
   return state
 }
